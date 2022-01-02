c221e2b45afaaa4798cd9cf0bc1e057dd1080ce0
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-01-02 12:21:26 +0100

    Inline addRI

--- src/Target/X86/Assembler.hs
+++ src/Target/X86/Assembler.hs
@@ -34,36 +34,33 @@ assembleInstructions =
 assembleInstruction :: Instruction -> MachineCodeBuilder
 assembleInstruction instruction =
   case instruction of
-    Add dst src -> do
-      let addRI :: Register -> Int64 -> MachineCodeBuilder
-          addRI r (toImm8 -> Just imm8) = do
-            let regWord = fromEnum8 r
-                rexReg = regWord `shiftR` 3
-                regOp = regWord .&. 7
-            word8 (0x48 .|. rexReg) -- REX prefix
-              <> word8 0x83 -- ADD
-              <> word8 (0xc0 .|. regOp)
-              <> word8 imm8
-          addRI RAX (toImm32 -> Just imm32) =
-            word8 0x48 -- REX prefix
-              <> word8 0x05 -- ADD
-              <> int32 imm32
-          addRI r (toImm32 -> Just imm32) = do
-            let regWord = fromEnum8 r
-                rexReg = regWord `shiftR` 3
-                regOp = regWord .&. 7
-            word8 (0x48 .|. rexReg) -- REX prefix
-              <> word8 0x81 -- ADD
-              <> word8 (0xc0 .|. regOp)
-              <> int32 imm32
-          addRI _ _ = error "add operand has to fit in 32 bits"
-
+    Add dst src ->
       case (dst, src) of
         (Register dstReg, Register srcReg) ->
           word8 0x48 -- REX prefix
             <> word8 0x01 -- ADD
             <> word8 (0xc0 .|. fromEnum8 srcReg .|. (fromEnum8 dstReg `shiftL` 3))
-        (Register r, Immediate i) -> addRI r i
+        (Register r, Immediate (toImm8 -> Just imm8)) -> do
+          let regWord = fromEnum8 r
+              rexReg = regWord `shiftR` 3
+              regOp = regWord .&. 7
+          word8 (0x48 .|. rexReg) -- REX prefix
+            <> word8 0x83 -- ADD
+            <> word8 (0xc0 .|. regOp)
+            <> word8 imm8
+        (Register RAX, Immediate (toImm32 -> Just imm32)) ->
+          word8 0x48 -- REX prefix
+            <> word8 0x05 -- ADD
+            <> int32 imm32
+        (Register r, Immediate (toImm32 -> Just imm32)) -> do
+          let regWord = fromEnum8 r
+              rexReg = regWord `shiftR` 3
+              regOp = regWord .&. 7
+          word8 (0x48 .|. rexReg) -- REX prefix
+            <> word8 0x81 -- ADD
+            <> word8 (0xc0 .|. regOp)
+            <> int32 imm32
+        (Register _, Immediate _) -> error "immediate operand has to fit in 32 bits"
         (Immediate _, _) -> error "immediate destination operand"
         _ -> mempty
     Call _ -> mempty
