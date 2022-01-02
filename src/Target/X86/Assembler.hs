53b51ce5f3d9300bf1cd4de02cfb19c1a344d2e4
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-01-02 12:58:51 +0100

    Call immediate apparently always uses 0 as the immediate

--- src/Target/X86/Assembler.hs
+++ src/Target/X86/Assembler.hs
@@ -69,10 +69,7 @@ assembleInstruction instruction =
         (Register _, Immediate _) -> error "immediate operand has to fit in 32 bits"
         (Immediate _, _) -> error "immediate destination operand"
         _ -> mempty
-    Call (Immediate (toImm32 -> Just imm32)) ->
-      word8 0xe8
-        <> int32 imm32
-    Call (Immediate _) -> error "immediate operand has to fit in 32 bits"
+    Call (Immediate _) -> word8 0xe8 <> int32 0
     Call _ -> mempty
     Ret -> word8 0xc3 -- RET
     Mov (Register r) (Immediate (toImm32 -> Just imm32)) -> do
