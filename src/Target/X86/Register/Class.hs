758510376eeb1335baeff0c292d7495bba797347
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-01-09 23:17:54 +0100

    Add class and constraint listings

--- src/Target/X86/Register/Class.hs
+++ src/Target/X86/Register/Class.hs
@@ -5,7 +5,7 @@ module Target.X86.Register.Class where
 
 import Data.Bits
 import Data.Word
-import Target.X86.Register
+import Target.X86.Assembly
 import Prelude hiding (any)
 
 data Class = Class {mask :: !Word8, pattern :: !Word8}
@@ -37,3 +37,54 @@ fromEnum8 x =
 
 instance FromRegister Class where
   fromRegister = exact
+
+mapWithClass :: (Class -> reg -> reg') -> Instruction reg -> Instruction reg'
+mapWithClass f instruction =
+  case instruction of
+    Add dst src1 src2 -> Add (f any <$> dst) (f any <$> src1) (f any <$> src2)
+    Mul (dst1, dst2) src1 src2 -> Mul (f rdx dst1, f rax dst2) (f rax src1) (f any <$> src2)
+    Call o -> Call $ f any <$> o
+    Ret -> Ret
+    Mov dst src -> Mov (f any <$> dst) (f any <$> src)
+
+data Constraint reg
+  = Same !reg !reg
+  | Unsatisifable
+  deriving (Eq, Show)
+
+constraints :: Eq reg => Instruction reg -> [Constraint reg]
+constraints instruction =
+  case instruction of
+    Add dst src1 _src2 -> sameOperands dst src1
+    Mul {} -> mempty
+    Call {} -> mempty
+    Ret -> mempty
+    Mov {} -> mempty
+
+sameRegisters :: Eq reg => reg -> reg -> [Constraint reg]
+sameRegisters reg1 reg2
+  | reg1 == reg2 = []
+  | otherwise = [Same reg1 reg2]
+
+sameOperands :: Eq reg => Operand reg -> Operand reg -> [Constraint reg]
+sameOperands (Immediate imm1) (Immediate imm2)
+  | imm1 == imm2 = []
+  | otherwise = [Unsatisifable]
+sameOperands (Immediate _) _ = [Unsatisifable]
+sameOperands (Register reg1) (Register reg2) = sameRegisters reg1 reg2
+sameOperands (Register _) _ = [Unsatisifable]
+sameOperands (Address addr1) (Address addr2) = sameAddresses addr1 addr2
+sameOperands (Address _) _ = [Unsatisifable]
+
+sameAddresses :: Eq reg => Address reg -> Address reg -> [Constraint reg]
+sameAddresses (Address' base1 index1 imm1) (Address' base2 index2 imm2)
+  | imm1 == imm2 =
+    case (base1, base2) of
+      (Nothing, Nothing) -> []
+      (Just reg1, Just reg2) -> sameRegisters reg1 reg2
+      _ -> [Unsatisifable]
+      <> case (index1, index2) of
+        (Nothing, Nothing) -> []
+        (Just (reg1, scale1), Just (reg2, scale2)) | scale1 == scale2 -> sameRegisters reg1 reg2
+        _ -> [Unsatisifable]
+  | otherwise = [Unsatisifable]
