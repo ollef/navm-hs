a4f2200237b32a5b9721b58fec20d3952a5a044a
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-01-30 23:45:39 +0100

    Add MachineCode.toByteString

--- src/Target/X86/MachineCode.hs
+++ src/Target/X86/MachineCode.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Target.X86.MachineCode where
 
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Unsafe as ByteString
 import Data.Primitive.PrimArray
+import Data.Primitive.Ptr
 import Data.Word
 import Text.Printf (printf)
 import Prelude hiding (max, min)
@@ -10,3 +15,8 @@ newtype MachineCode = MachineCode (PrimArray Word8)
 instance Show MachineCode where
   showsPrec _ (MachineCode arr) =
     foldrPrimArray (\c -> (showString (printf "%02x " c) .)) id arr
+
+toByteString :: MachineCode -> IO ByteString
+toByteString (MachineCode arr) = ByteString.unsafePackAddressLen (sizeofPrimArray arr) addr
+  where
+    !(Ptr addr) = primArrayContents arr
