09f438e1ca95cdb709484f723053100d50473687
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-01-30 23:23:29 +0100

    Show MachineCode in hex

--- src/Target/X86/MachineCode.hs
+++ src/Target/X86/MachineCode.hs
@@ -2,7 +2,11 @@ module Target.X86.MachineCode where
 
 import Data.Primitive.PrimArray
 import Data.Word
+import Text.Printf (printf)
 import Prelude hiding (max, min)
 
 newtype MachineCode = MachineCode (PrimArray Word8)
-  deriving (Show)
+
+instance Show MachineCode where
+  showsPrec _ (MachineCode arr) =
+    foldrPrimArray (\c -> (showString (printf "%02x " c) .)) id arr
