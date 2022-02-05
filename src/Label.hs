00d7e89056f90499150f6b0ffcf8adc73097c472
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-02-05 13:01:33 +0100

    Add IsString Label instance

--- src/Label.hs
+++ src/Label.hs
@@ -4,6 +4,7 @@ module Label where
 
 import Data.ByteString
 import Data.Hashable
+import Data.String
 
 newtype Label = Label ByteString
-  deriving (Eq, Show, Hashable)
+  deriving (Eq, Show, Hashable, IsString)
