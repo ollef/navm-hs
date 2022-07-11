c7b277b160fa626517828495e1c5a873c7633e7c
Olle Fredriksson <fredriksson.olle@gmail.com>
2022-07-11 20:32:24 +0200

    Revamp mapWithClass

--- src/Target/X86/Register/Class.hs
+++ src/Target/X86/Register/Class.hs
@@ -37,64 +37,26 @@ type instance RegisterType Class = Register
 instance FromRegister Class where
   fromRegister = exact
 
-mapWithClass :: (Class -> reg -> reg') -> Instruction reg -> Instruction reg'
-mapWithClass f instruction =
-  case instruction of
-    Add dst src1 src2 -> Add (f any <$> dst) (f any <$> src1) (f any <$> src2)
-    Mul (dst1, dst2) src1 src2 -> Mul (f rdx dst1, f rax dst2) (f rax src1) (f any <$> src2)
-    Jmp o -> Jmp (f any <$> o)
-    Call o -> Call $ f any <$> o
-    Ret -> Ret
-    Mov dst src -> Mov (f any <$> dst) (f any <$> src)
-    MovImmediate64 dst src -> MovImmediate64 (f any dst) src
-    Define label -> Define label
-
-data Constraint reg
-  = Same !reg !reg
+data Occurrence = Definition | Use
   deriving (Eq, Show)
 
-constraints :: Eq reg => Instruction reg -> Maybe [Constraint reg]
-constraints instruction =
+mapWithClass ::
+  Monad m =>
+  (Occurrence -> Class -> reg -> m reg') ->
+  Instruction reg ->
+  m (Instruction reg')
+mapWithClass f instruction =
   case instruction of
-    Add dst src1 _src2 -> sameOperands dst src1
-    Mul {} -> Just mempty
-    Jmp {} -> Just mempty
-    Call {} -> Just mempty
-    Ret -> Just mempty
-    Mov {} -> Just mempty
-    MovImmediate64 {} -> Just mempty
-    Define {} -> Just mempty
-
-sameRegisters :: Eq reg => reg -> reg -> [Constraint reg]
-sameRegisters reg1 reg2
-  | reg1 == reg2 = []
-  | otherwise = [Same reg1 reg2]
-
-sameOperands :: Eq reg => Operand reg -> Operand reg -> Maybe [Constraint reg]
-sameOperands (Immediate imm1) (Immediate imm2)
-  | imm1 == imm2 = Just []
-  | otherwise = Nothing
-sameOperands (Immediate _) _ = Nothing
-sameOperands (Register reg1) (Register reg2) = Just $ sameRegisters reg1 reg2
-sameOperands (Register _) _ = Nothing
-sameOperands (Memory addr1) (Memory addr2) = sameAddresses addr1 addr2
-sameOperands (Memory _) _ = Nothing
-
-sameAddresses :: Eq reg => Address reg -> Address reg -> Maybe [Constraint reg]
-sameAddresses (Address base1 label1 imm1) (Address base2 label2 imm2)
-  | imm1 == imm2 && label1 == label2 =
-      sameBases base1 base2
-  | otherwise = Nothing
-
-sameBases :: Eq reg => Base reg -> Base reg -> Maybe [Constraint reg]
-sameBases (Absolute base1 index1) (Absolute base2 index2) =
-  case (base1, base2) of
-    (Nothing, Nothing) -> Just []
-    (Just reg1, Just reg2) -> Just $ sameRegisters reg1 reg2
-    _ -> Nothing
-    <> case (index1, index2) of
-      (Nothing, Nothing) -> Just []
-      (Just (reg1, scale1), Just (reg2, scale2)) | scale1 == scale2 -> Just $ sameRegisters reg1 reg2
-      _ -> Nothing
-sameBases Relative Relative = Just []
-sameBases _ _ = Nothing
+    Add dst src1 src2 -> Add <$> def any dst <*> use any src1 <*> use any src2
+    Mul (dst1, dst2) src1 src2 ->
+      Mul <$> ((,) <$> f Definition rdx dst1 <*> f Definition rax dst2) <*> f Use rax src1 <*> use any src2
+    Jmp o -> Jmp <$> mapM (f Use any) o
+    Call o -> Call <$> mapM (f Use any) o
+    Ret -> pure Ret
+    Mov dst src -> Mov <$> def any dst <*> use any src
+    MovImmediate64 dst src -> MovImmediate64 <$> f Definition any dst <*> pure src
+    Define label -> pure $ Define label
+  where
+    use class_ operand = mapM (f Use class_) operand
+    def class_ (Register r) = Register <$> f Definition class_ r
+    def class_ operand = use class_ operand
